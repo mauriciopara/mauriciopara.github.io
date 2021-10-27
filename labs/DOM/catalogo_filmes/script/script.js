c1b1e513f01d566a00cf6d94f209daa7b03db4a5
Mauricio Pará Roza Muniz de Aguiar <paramauricio7@gmail.com>
2021-10-27 14:43:22 -0300

    Imagens

--- labs/DOM/catalogo_filmes/script/script.js
+++ labs/DOM/catalogo_filmes/script/script.js
@@ -91,7 +91,7 @@ let url = `https://rafaelescalfoni.github.io/desenv_web/filmes.json`;
 
                 for(let j = 1; j <= aux; j++){
                 let img = document.createElement("img");
-                img.src= "/img/star.svg";
+                img.src= "/imagens/chocolate.jpg";
                 total.appendChild(img)
                 }
                 
